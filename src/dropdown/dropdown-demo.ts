85838a17bcf5c0ba01b901fd68f8163725994c76
srikarpohar <srikarpohar@gmail.com>
2026-03-17 09:19:09 +0530

    minor changes

--- src/dropdown/dropdown-demo.ts
+++ src/dropdown/dropdown-demo.ts
@@ -10,33 +10,33 @@ for(let i=0;i < 15000;i++) {
 }
 
 
-// const virtualScrollDropdown = document.createElement('drop-down') as any;
-// virtualScrollDropdown.setAttribute('item-height', '30px');
-// virtualScrollDropdown.setAttribute('initial-items-no', '10');
-// virtualScrollDropdown.setAttribute('no-data-text', "No data available");
-// virtualScrollDropdown.setAttribute('use-virtual-scroll', 'true');
+const virtualScrollDropdown = document.createElement('drop-down') as any;
+virtualScrollDropdown.setAttribute('item-height', '30px');
+virtualScrollDropdown.setAttribute('initial-items-no', '10');
+virtualScrollDropdown.setAttribute('no-data-text', "No data available");
+virtualScrollDropdown.setAttribute('use-virtual-scroll', 'true');
 
-// virtualScrollDropdown.setOptions(options);
-// virtualScrollDropdown.setItemClick((option: IOption) => console.log(option));
-// document.querySelector(".virtual-scroll-dropdown")?.appendChild(virtualScrollDropdown);
+virtualScrollDropdown.setOptions(options);
+virtualScrollDropdown.setItemClick((option: IOption) => console.log(option));
+document.querySelector(".virtual-scroll-dropdown")?.appendChild(virtualScrollDropdown);
 
 
-const infiniteScrollDropdown = document.createElement('drop-down') as any;
-infiniteScrollDropdown.setAttribute('no-data-text', "No data available");
-infiniteScrollDropdown.setAttribute('use-infinite-scroll', 'true');
-infiniteScrollDropdown.setAttribute('items-limit', '40');
+// const infiniteScrollDropdown = document.createElement('drop-down') as any;
+// infiniteScrollDropdown.setAttribute('no-data-text', "No data available");
+// infiniteScrollDropdown.setAttribute('use-infinite-scroll', 'true');
+// infiniteScrollDropdown.setAttribute('items-limit', '40');
 
-infiniteScrollDropdown.setOptions(options.slice(0, 40));
-infiniteScrollDropdown.setItemClick((option: IOption) => console.log(option));
-infiniteScrollDropdown.setInfiniteScrollFn((page: number, limit: number, searchValue: string) => {
-    const start = page * limit,
-        end = Math.min(limit + page * limit, options.length);
+// infiniteScrollDropdown.setOptions(options.slice(0, 40));
+// infiniteScrollDropdown.setItemClick((option: IOption) => console.log(option));
+// infiniteScrollDropdown.setInfiniteScrollFn((page: number, limit: number, searchValue: string) => {
+//     const start = page * limit,
+//         end = Math.min(limit + page * limit, options.length);
 
-    let finalOptions = options;
-    if(searchValue) {
-        const regEx = new RegExp(searchValue, 'i');
-        finalOptions = options.filter(doc => regEx.test(doc.label));
-    }
-    return finalOptions.slice(start, end);
-});
-document.querySelector(".infinite-scroll-dropdown")?.appendChild(infiniteScrollDropdown);
+//     let finalOptions = options;
+//     if(searchValue) {
+//         const regEx = new RegExp(searchValue, 'i');
+//         finalOptions = options.filter(doc => regEx.test(doc.label));
+//     }
+//     return finalOptions.slice(start, end);
+// });
+// document.querySelector(".infinite-scroll-dropdown")?.appendChild(infiniteScrollDropdown);
